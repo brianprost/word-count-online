abe80b305424595553536c5ad7d4f607238562f2
Brian Prost <brian.s.prost@gmail.com>
2022-03-30 15:00:46 -0400

    daisyui styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,9 +18,21 @@ const Home: NextPage = () => {
 
   return (
     <div className="container mx-auto flex h-screen flex-col items-center justify-center">
-      <input className="" value={text} onChange={textHandler} />
-
-      <h2>{wordCount}</h2>
+      <input
+        className="input input-bordered input-lg w-full max-w-xs"
+        value={text}
+        onChange={textHandler}
+      />
+      <div className="stats stats-vertical shadow lg:stats-horizontal">
+        <div className="stat">
+          <div className="stat-title">Words</div>
+          <div className="stat-value">{wordCount}</div>
+        </div>
+        <div className="stat">
+          <div className="stat-title">Time to read</div>
+          <div className="stat-value">{Math.ceil(wordCount / 255)} Minutes</div>
+        </div>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,25 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": '#3ec9a4',
+          "secondary": '#dbd655',
+          "accent": '#3457a8',
+          "neutral": '#191424',
+          "base-100": '#464346',
+          "info": '#3F6AE9',
+          "success": '#1E7B53',
+          "warning": '#CCA405',
+          "error": '#F2726E',
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"), require('daisyui')],
 }
